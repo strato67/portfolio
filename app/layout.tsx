5842d97cfb701a64b2a19e0cf9f4f81f961e316b
strato67 <saaranshsharma28@gmail.com>
2024-07-02 22:36:50 -0400

    adding smooth scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         <div className="absolute top-0 z-[-2] w-screen bg-neutral-900 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
           {children}
