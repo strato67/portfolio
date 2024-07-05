a38a71b4738881fe11bff937b62d08e3de6faf02
strato67 <saaranshsharma28@gmail.com>
2024-07-04 21:00:33 -0400

    adding navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>
         <div className="absolute top-0 z-[-2] w-screen bg-neutral-900 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+
           {children}
         </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
-"use client"
+"use client";
 
 import Home from "./home";
 import About from "./about";
 import Skills from "./skills";
 import Projects from "./projects";
+import Navbar from "@/components/ui/navbar";
 
 export default function Page() {
-  return (<>
-    <Home />
-    <About />
-    <Skills />
-    <Projects />
-  </>)
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Projects />
+    </>
+  );
 }
-
